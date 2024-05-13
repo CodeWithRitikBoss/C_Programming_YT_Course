0255a6aff6b25df7e7752d20d11d65e89f45e61a
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-05-13 11:34:28 +0530

    Code commited.

---
+++ Demo.c
@@ -0,0 +1,37 @@
+// // C Program to Convert Inches to Centimeters
+// #include <stdio.h>
+// int main(){
+//     float inches, cm;
+    
+//     // Asking for input
+//     printf("Enter the length in inches: ");
+//     scanf("%f", &inches);
+    
+//     // Inches to Centimeters
+//     cm = inches * 2.54;
+    
+//     // Display output
+//     printf("%.2f Inches = %.2f Centimeters", inches, cm);
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    
+    // Asking for input
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    // Adding the numbers
+    sum = num1 + num2;
+    
+    // Displaying the sum
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+    
+    return 0;
+}
