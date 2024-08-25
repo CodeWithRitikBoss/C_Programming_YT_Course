ec7537531aafa08053d2e780614243621fcf87e4
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-08-26 01:01:25 +0530

    Code commited on 26-08-2024

---
+++ GotoXY.c
@@ -0,0 +1,22 @@
+//Code a program to use gotoxy function in C.
+#include"stdio.h"
+#include<windows.h>
+
+void gotoxy(int, int);
+
+int main(){
+    gotoxy(5, 10);
+    printf("Gotoxy function in C.\n\n");
+    
+    gotoxy(10, 15);
+    printf("Welcome");
+    
+    return(0);
+}
+
+void gotoxy(int x, int y){
+    COORD c;
+    c.X= x;
+    c.Y= y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
